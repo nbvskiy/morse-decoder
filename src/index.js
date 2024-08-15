cc16aa3a35664443b44910af16923b4977474936
Ruslan <nabv.dev@gmail.com>
2024-08-15 20:44:21 +0600

    add: solution to morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decodedString = '';
+    
+    // Проходим по строке с шагом 10
+    for (let i = 0; i < expr.length; i += 10) { // Замените input на expr
+        const letterCode = expr.substring(i, i + 10);
+        
+        // Если это пробел
+        if (letterCode === '**********') {
+            decodedString += ' ';
+        } else {
+            // Преобразуем в морзянку
+            let morseLetter = '';
+            for (let j = 0; j < letterCode.length; j += 2) {
+                const codePair = letterCode.substring(j, j + 2);
+                if (codePair === '10') {
+                    morseLetter += '.';
+                } else if (codePair === '11') {
+                    morseLetter += '-';
+                }
+            }
+            // Добавляем соответствующую букву
+            if (MORSE_TABLE[morseLetter]) {
+                decodedString += MORSE_TABLE[morseLetter];
+            }
+        }
+    }
+
+    return decodedString;
 }
 
 module.exports = {
